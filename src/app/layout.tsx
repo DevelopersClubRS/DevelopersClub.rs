36d94d175cca932beda4104be2d9db76bc22c1ed
Aleksandar Sabo <alxsabo@gmail.com>
2025-04-07 23:43:07 +0200

    Add analytics to website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Developers Club',
-  description: 'Mesto okupljanja IT profesionalaca, gde se razmenjuju ideje, iskustva i znanje.',
+  description: 'Mesto okupljanja IT profesionalaca, gde se razmenjuju ideje, iskustva i znanje.'
 }
 
 export default function RootLayout({
@@ -16,6 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="sr">
+      <head>
+        <script defer src="https://cloud.umami.is/script.js" data-website-id="18045f12-37bd-4c05-857e-68f6f7b29f01"></script>
+      </head>
       <body className={inter.className}>
         {children}
       </body>
